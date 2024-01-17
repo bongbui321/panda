45a28e7a891856b7c35070f595a19471fb986caa
bongbui321 <hoang.bui.vh@gmail.com>
2024-01-17 17:47:54 -0500

    cig

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -16,11 +16,13 @@ fi
 
 # ensure checked in coverage table is up to date
 cd $DIR
-python $CPPCHECK_DIR/addons/misra.py -generate-table > new_table
-if ! cmp -s new_table coverage_table; then
-  echo "MISRA coverage table doesn't match. Update and commit:"
-  echo "mv new_table coverage_table && git add . && git commit -m 'update table'"
-  exit 1
+if [ -z "${CI}" ]; then
+  python $CPPCHECK_DIR/addons/misra.py -generate-table > new_table
+  if ! cmp -s new_table coverage_table; then
+    echo "MISRA coverage table doesn't match. Update and commit:"
+    echo "mv new_table coverage_table && git add . && git commit -m 'update table'"
+    exit 1
+  fi
 fi
 
 cd $PANDA_DIR
