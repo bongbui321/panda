b64a13962acb0311ce39b176fb17e4e6f12b4137
bongbui321 <hoang.bui.vh@gmail.com>
2024-07-26 17:39:14 -0400

    fix

--- tests/usbprotocol/test_comms.py
+++ tests/usbprotocol/test_comms.py
@@ -1,3 +1,4 @@
+import pytest
 import random
 
 from panda import Panda, DLC_TO_LEN, USBPACKET_MAX_SIZE, pack_can_buffer, unpack_can_buffer
@@ -98,29 +99,28 @@ class TestPandaComms:
       assert m == test_msg, "message buffer should contain valid test messages"
 
 
-  def test_can_send_usb(self):
+  @pytest.mark.parameterize("bus", range(3))
+  def test_can_send_usb(self, bus):
     lpp.set_safety_hooks(Panda.SAFETY_ALLOUTPUT, 0)
 
-    for bus in range(3):
-      with self.subTest(bus=bus):
-        for _ in range(100):
-          msgs = random_can_messages(200, bus=bus)
-          packed = pack_can_buffer(msgs)
+    for _ in range(100):
+      msgs = random_can_messages(200, bus=bus)
+      packed = pack_can_buffer(msgs)
 
-          # Simulate USB bulk chunks
-          for buf in packed:
-            for i in range(0, len(buf), CHUNK_SIZE):
-              chunk_len = min(CHUNK_SIZE, len(buf) - i)
-              lpp.comms_can_write(buf[i:i+chunk_len], chunk_len)
+      # Simulate USB bulk chunks
+      for buf in packed:
+        for i in range(0, len(buf), CHUNK_SIZE):
+          chunk_len = min(CHUNK_SIZE, len(buf) - i)
+          lpp.comms_can_write(buf[i:i+chunk_len], chunk_len)
 
-          # Check that they ended up in the right buffers
-          queue_msgs = []
-          pkt = libpanda_py.ffi.new('CANPacket_t *')
-          while lpp.can_pop(TX_QUEUES[bus], pkt):
-            queue_msgs.append(unpackage_can_msg(pkt))
+      # Check that they ended up in the right buffers
+      queue_msgs = []
+      pkt = libpanda_py.ffi.new('CANPacket_t *')
+      while lpp.can_pop(TX_QUEUES[bus], pkt):
+        queue_msgs.append(unpackage_can_msg(pkt))
 
-          assert len(queue_msgs) == len(msgs)
-          assert queue_msgs == msgs
+      assert len(queue_msgs) == len(msgs)
+      assert queue_msgs == msgs
 
   def test_can_receive_usb(self):
     msgs = random_can_messages(50000)
